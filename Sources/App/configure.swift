a075768ac49ecccd685c9f37a9b01ae608cea233
Christine Abernathy <ca.abernathy@gmail.com>
2019-01-01 13:07:49 -0500

    Can also configure database from URL

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -18,17 +18,22 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
   services.register(middlewares)
   
   // Configure a PostgreSQL database
-  let hostname = Environment.get("DATABASE_HOSTNAME")
-    ?? "localhost"
-  let username = Environment.get("DATABASE_USER") ?? "vapor"
-  let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-  let password = Environment.get("DATABASE_PASSWORD")
-    ?? "password"
-  let databaseConfig = PostgreSQLDatabaseConfig(
-    hostname: hostname,
-    username: username,
-    database: databaseName,
-    password: password)
+  let databaseConfig:PostgreSQLDatabaseConfig
+  if let url = Environment.get("DATABASE_URL") {
+    databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+  } else {
+    let hostname = Environment.get("DATABASE_HOSTNAME")
+      ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD")
+      ?? "password"
+    databaseConfig = PostgreSQLDatabaseConfig(
+      hostname: hostname,
+      username: username,
+      database: databaseName,
+      password: password)
+  }
   let postgreSQL = PostgreSQLDatabase(config: databaseConfig)
   
   /// Register the configured SQLite database to the database config.
